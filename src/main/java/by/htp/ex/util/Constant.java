b9d2312802ec10adb91a394ca806b2a9852d6537
JohnyRazDva <39062373+JohnyRazDva@users.noreply.github.com>
2022-08-10 20:15:10 +0300

    Add files via upload

--- src/main/java/by/htp/ex/util/Constant.java
+++ src/main/java/by/htp/ex/util/Constant.java
@@ -1,6 +1,9 @@
 package by.htp.ex.util;
 
-public class Constant {
+public final class Constant {
+	private Constant() {
+	};
+
 	public static final String SQL_USER_INSERT = "INSERT INTO user (login, password, email, access) VALUES (?,?,?,?)";
 	public static final String SQL_USER_SELECT_BY_LOGIN_PASSWORD = "SELECT * FROM user WHERE login=? AND password=?";
 
